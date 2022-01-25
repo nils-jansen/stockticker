2c22d798279f1ece6e8ad7bbf9b77d53ce5846eb
Nils Jansen <nils@fabbits.de>
2022-01-25 21:10:47 +0100

    Form component working

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -4,7 +4,58 @@ interface FormProps {
 
 function Form(props: FormProps) {
   return (
-    <div className="h-full w-full flex items-center justify-center">Form</div>
+    <div className="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+          Choose a stock symbol
+        </h2>
+      </div>
+
+      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+        <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+          <form
+            className="space-y-6"
+            onSubmit={(e) => {
+              e.preventDefault();
+
+              const textField = document.getElementById(
+                "symbol"
+              ) as HTMLInputElement;
+
+              props.onSymbolSelect(textField.value);
+            }}
+          >
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Symbol (e.g. AAPL)
+              </label>
+              <div className="mt-1">
+                <input
+                  id="symbol"
+                  name="symbol"
+                  type="text"
+                  autoComplete="none"
+                  required
+                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-teal-500 focus:border-teal-500 sm:text-sm"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-teal-600 hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500"
+              >
+                Start live view
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 }
 
