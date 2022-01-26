46736e4b3baa3c1a8d676b0a7610e7b1ba0c71e8
Nils Jansen <nils@fabbits.de>
2022-01-26 21:41:54 +0100

    Autofocus input

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -39,6 +39,7 @@ function Form(props: FormProps) {
                   type="text"
                   autoComplete="none"
                   required
+                  autoFocus
                   className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-teal-500 focus:border-teal-500 sm:text-sm"
                 />
               </div>
